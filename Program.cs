7b1f05bb5aae1f8dffff33a2d08cb63a4099dc7d
Leon <mail@leonkuehn.de>
2025-08-25 22:28:01 +0200

    modified program

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using VacationPlanner.Data;
+
+var connectionString = "Data Source=vacationplanner.db";
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,11 +11,14 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(connectionString));
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // The default HSTS value is 30 Sdays. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
